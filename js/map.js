c80c59057d7112cbfc6872e5fd7e1cccd4a62319
Nicholas <nicholasrolstad@outlook.com>
2019-03-06 15:08:10 -0800

    general update

--- js/map.js
+++ js/map.js
@@ -1,118 +1,126 @@
+$(document).ready(function(){
+	//
+	// initiate map
+	//
 
-//
-// initiate map
-//
+	var map = L.map('map', {zoomControl: false}).setView([45.0, -93.3], 8),
+		layer = L.esri.basemapLayer("Gray").addTo(map);
+		//layerLabels = L.esri.basemapLayer('GrayLabels').addTo(map);
+		layerLabels = null;
 
-var map = L.map('map', {zoomControl: false}).setView([45.0, -93.3], 8),
-  layer = L.esri.basemapLayer("Gray").addTo(map);
-  //layerLabels = L.esri.basemapLayer('GrayLabels').addTo(map);
-  layerLabels = null;
+	function setBasemap(basemap) {
+		if (layer) {
+		map.removeLayer(layer);
+		}
+		if (basemap === 'WHS') {
+		L.esri.basemapLayer("Gray").addTo(map);
+		}
+		else {
+		layer = L.esri.basemapLayer(basemap);
+		}
+		map.addLayer(layer);
+		if (layerLabels) {
+		map.removeLayer(layerLabels);
+		}
+		if (basemap === 'ShadedRelief' || basemap === 'Oceans' || basemap === 'DarkGray' || basemap === 'Imagery' || basemap === 'Terrain') {
+		layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+		map.addLayer(layerLabels);
+		}
+	}
 
-function setBasemap(basemap) {
-  if (layer) {
-	map.removeLayer(layer);
-  }
-  if (basemap === 'WHS') {
-	L.esri.basemapLayer("Gray").addTo(map);
-  }
-  else {
-	layer = L.esri.basemapLayer(basemap);
-  }
-  map.addLayer(layer);
-  if (layerLabels) {
-	map.removeLayer(layerLabels);
-  }
-  if (basemap === 'ShadedRelief' || basemap === 'Oceans' || basemap === 'DarkGray' || basemap === 'Imagery' || basemap === 'Terrain') {
-	layerLabels = L.esri.basemapLayer(basemap + 'Labels');
-	map.addLayer(layerLabels);
-  }
-}
-
-L.control.zoom({
-  position:'topright'
-}).addTo(map);
-
-
-//
-// style functions
-//
+	L.control.zoom({
+		position:'topright'
+	}).addTo(map);
 
 
+	//
+	// style functions
+	//
 
-//
-// feature layers
-//
 
-var point_icon = L.icon({
-	iconUrl: 'img/icon.svg',
-	iconSize: [30,30]
-})
 
-var points = L.esri.featureLayer({
-		url: 'https://services9.arcgis.com/YEQ7YfprtcM3j3JL/ArcGIS/rest/services/MN_CSG_2_view/FeatureServer/0',
-		pointToLayer: function (geojson, latlng) {
-			return L.marker(latlng, {
-				icon: point_icon
-			});
-		},
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.Name);
-        }
-	});
+	//
+	// feature layers
+	//
 
-var premises = L.esri.featureLayer({
-		url: 'https://services9.arcgis.com/YEQ7YfprtcM3j3JL/arcgis/rest/services/MN_CSG_2_view/FeatureServer/1',
-		style: {
-			weight: 1,
-			color: 'orange'
+	var point_icon = L.icon({
+		iconUrl: 'img/icon.svg',
+		iconSize: [30,30]
+	})
+
+	var points = L.esri.featureLayer({
+			url: 'https://services9.arcgis.com/YEQ7YfprtcM3j3JL/ArcGIS/rest/services/MN_CSG_2_view/FeatureServer/0',
+			pointToLayer: function (geojson, latlng) {
+				return L.marker(latlng, {
+					icon: point_icon
+				});
+			},
+					onEachFeature: function(feature, layer) {
+							layer.bindPopup(feature.properties.Name);
+					}
+		});
+
+	var premises = L.esri.featureLayer({
+			url: 'https://services9.arcgis.com/YEQ7YfprtcM3j3JL/arcgis/rest/services/MN_CSG_2_view/FeatureServer/1',
+			style: {
+				weight: 1,
+				color: 'orange'
+			}
+		});
+
+
+
+
+
+	//
+	// initiate layers
+	//
+
+	points.addTo(map);
+
+
+
+	map.on('zoomend', function() {
+			if (map.getZoom() <15){
+			points.addTo(map);
+		} else {
+			map.removeLayer(points);
 		}
 	});
 
+	map.on('zoomend', function() {
+			if (map.getZoom() <15){
+			map.removeLayer(premises);
+		} else {
+			premises.addTo(map);
+		}
+	});
 
-
-
-
-//
-// initiate layers
-//
-
-points.addTo(map);
-
-
-
-map.on('zoomend', function() {
-    if (map.getZoom() <15){
-		points.addTo(map);
-	} else {
-		map.removeLayer(points);
-	}
-});
-	   
-map.on('zoomend', function() {
-    if (map.getZoom() <15){
-		map.removeLayer(premises);
-	} else {
-		premises.addTo(map);
-	}
+	map.on('mousedown', function(e) {
+		
+		
+			if ( $('#collapseBasemaps').hasClass("in")) {
+				$('#collapseBasemaps').removeClass('in');
+			};
 });
 
 
 
 
 
-    //var searchControl = L.esri.Geocoding.Controls.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
-    var searchControl = L.esri.Geocoding.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
-    
-    searchControl.on('results', function(data){ 
-      if (data.results.length > 0) {
-        var popup = L.popup()
-          .setLatLng(data.results[0].latlng)
-          .setContent(data.results[0].text)
-          .openOn(map);
-        map.setView(data.results[0].latlng)
-		console.log(data.results[0].latlng)
-      }
-    })
+			//var searchControl = L.esri.Geocoding.Controls.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
+			var searchControl = L.esri.Geocoding.geosearch({expanded: true, collapseAfterResult: false, zoomToResult: false}).addTo(map);
+
+			searchControl.on('results', function(data){ 
+				if (data.results.length > 0) {
+					var popup = L.popup()
+						.setLatLng(data.results[0].latlng)
+						.setContent(data.results[0].text)
+						.openOn(map);
+					map.setView(data.results[0].latlng)
+			console.log(data.results[0].latlng)
+				}
+			});
 
 
 
@@ -120,45 +128,45 @@ map.on('zoomend', function() {
 
 
 
-//
-// jQuery listeners
-//
+	//
+	// jQuery listeners
+	//
 
-$(document).ready(function(){
-	$("#selectStandardBasemap").on("change", function(e) {
-		setBasemap($(this).val());
-  	});
-    
-    
-    $('#operating-btn').click(function () {
-			$('#operating-btn').toggleClass('btn-primary');
-			$('#operating-btn').toggleClass('btn-secondary');	
-		});
-	
-	$('#future-btn').click(function () {
-			$('#future-btn').toggleClass('btn-primary');
-			$('#future-btn').toggleClass('btn-secondary');
-		});
-    
-	
-	
-	// Search
-      var input = $(".geocoder-control-input");
-      input.focus(function(){
-        $("#panelSearch .panel-body").css("height", "150px");
-      });
-      input.blur(function(){
-         $("#panelSearch .panel-body").css("height", "auto");
-      });
-      // Attach search control for desktop or mobile
-      function attachSearch() {
-        var parentName = $(".geocoder-control").parent().attr("id"),
-          geocoder = $(".geocoder-control"),
-          width = $(window).width();
-          $("#geocode").append(geocoder);
-      };
-      attachSearch();
-	
+
+		$("#selectStandardBasemap").on("change", function(e) {
+			setBasemap($(this).val());
+			});
+
+
+			$('#operating-btn').click(function () {
+				$('#operating-btn').toggleClass('btn-info');
+				$('#operating-btn').toggleClass('btn-secondary');	
+			});
+
+		$('#future-btn').click(function () {
+				$('#future-btn').toggleClass('btn-info');
+				$('#future-btn').toggleClass('btn-secondary');
+			});
+
+
+
+		// Search
+				var input = $(".geocoder-control-input");
+				input.focus(function(){
+					$("#panelSearch .panel-body").css("height", "auto");
+				});
+				input.blur(function(){
+					 $("#panelSearch .panel-body").css("height", "auto");
+				});
+				// Attach search control for desktop or mobile
+				function attachSearch() {
+					var parentName = $(".geocoder-control").parent().attr("id"),
+						geocoder = $(".geocoder-control"),
+						width = $(window).width();
+						$("#geocode").append(geocoder);
+				}
+				attachSearch();
+
 
 });
 
